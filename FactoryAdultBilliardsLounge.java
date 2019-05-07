52197357b314997d4ad47221f84455966ab1f4cb
Justin Mabutas <justin.troy.mabutas@gmail.com>
2019-05-07 12:26:27 -0700

    Javadoc'd

--- FactoryAdultBilliardsLounge.java
+++ FactoryAdultBilliardsLounge.java
@@ -1,4 +1,8 @@
+
 public class FactoryAdultBilliardsLounge {
+	/**
+	 * createRoom function which returns the room as a BaseRoom
+	*/
 	public BaseRoom createRoom() {
 		return new KaraokeLounge();
 	}
