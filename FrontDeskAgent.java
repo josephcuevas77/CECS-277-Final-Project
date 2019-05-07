1ff1c6d010fed84aab17a2d1093c23cb194a2c75
Joseph Cuevas <38505353+josephcuevas77@users.noreply.github.com>
2019-05-07 12:48:55 -0700

    Javadoc'd

--- FrontDeskAgent.java
+++ FrontDeskAgent.java
@@ -3,24 +3,54 @@ package FinalExam;
 import java.util.ArrayList;
 import java.util.Scanner;
 
+/**
+ * Master class/Employee that has control over all rooms, guest info, and reservations
+ *
+ */
 public class FrontDeskAgent {
 	
+	private Scanner in = new Scanner(System.in);
+	
 	private ArrayList<Guest> guests = new ArrayList<Guest>();
-	private ArrayList<Reservation> reservations = new ArrayList<Reservation>();
+	private static ArrayList<Reservation> reservations = new ArrayList<Reservation>();
 	private ArrayList<Reservation> waitlist = new ArrayList<Reservation>();
 
-	public boolean viewAvailability() {
+	/**
+	 * Views if room is available at the time requested
+	 * @return boolean of true if room is free and false if otherwise
+	 */
+	public boolean viewAvailability(Guest guest) {
+		System.out.print("\nEnter the Date of Reservation: MM/DD/YYYY: ");
+		String[] stringDate = in.nextLine().split("/");
+		int[] date = new int[stringDate.length];
+		for(int i = 0; i < stringDate.length; i++) date[i] = Integer.parseInt(stringDate[i]);
+		guest.setDate(new Date(date[0], date[1], date[2]));
 		
+		for(Reservation i: reservations) {
+			return(guest.getDate().equals(i.getDate()));
+		}
 	}
 	
+	/**
+	 * Adds reservation to waitlist if full
+	 * @param r - reservation to be added
+	 */
 	public void addToWaitlist(Reservation r) {
 		waitlist.add(r);
 	}
 	
+	/**
+	 * Removes reservation from waitlist
+	 * @param r - reservation
+	 */
 	public void removeFromWaitlist(Reservation r) {
 		waitlist.remove(r);
 	}
 	
+	/**
+	 * updates the guest info
+	 * @param g - guest
+	 */
 	public void updateGuestInfo(Guest g) {
 		Scanner in = new Scanner(System.in);
 		if (choice == 1){
@@ -28,8 +58,13 @@ public class FrontDeskAgent {
 		}
 	}
 	
+	/**
+	 * checks if a room is reserved
+	 * @param d - date of reservation
+	 * @return true if reserved and false otherwise
+	 */
 	public boolean isReserved(Date d) {
-		for (Reservation r : reservations) {
+		for (Reservation r : getReservations()) {
 			if (r.getDate() == d) {
 				return true;
 			}
@@ -37,16 +72,31 @@ public class FrontDeskAgent {
 		return false;
 	}
 	
+	/**
+	 * Collects money from customer
+	 */
 	public void collectAmountDue() {
 		
 	}
 	
+	/**
+	 * edits the info of guest
+	 */
 	public void editGuestInfo() {
 		
 	}
 	
+	
 	public static void main(String[] args) {
 
+	}
+
+	/**
+	 * Gets the list of reservations
+	 * @return
+	 */
+	public static ArrayList<Reservation> getReservations() {
+		return reservations;
 	}
 
 }
