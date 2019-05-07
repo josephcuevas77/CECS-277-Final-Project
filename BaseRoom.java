8761e7c9f814e75c213918c4c74b1670fb55668c
joeytran99 <50354428+joeytran99@users.noreply.github.com>
2019-05-07 12:30:45 -0700

    javadocced

--- BaseRoom.java
+++ BaseRoom.java
@@ -1,5 +1,15 @@
+package FinalExam;
+
+/**
+ * The Super class of all the individual rooms to be reserved
+ * Has the instance variables that all the rooms share
+ */
+
 public abstract class BaseRoom {
 
+	/**
+	 * default constructor
+	 */
     public BaseRoom() {
         description = "";
         capacity = 0;
@@ -7,6 +17,13 @@ public abstract class BaseRoom {
         mealPlan = new MealPlan();
     }
     
+    /**
+     * overloaded constructor
+     * @param desc - description of room
+     * @param capa - max room capacity
+     * @param cost - cost of the room per hour
+     * @param mp - meal plan for the party
+     */
     public BaseRoom(String desc, int capa, double cost, MealPlan mp) {
         description = desc;
         capacity = capa;
@@ -14,11 +31,23 @@ public abstract class BaseRoom {
         mealPlan = mp;
     }
     
+    /**
+     * instance variables
+     */
     private String description;
     private int capacity;
     private double costPerHour;
     private MealPlan mealPlan;
 
+    /**
+     * gets the final cost after how many hours the room has been used
+     * and the meal plan bought
+     * @return the final cost of the room
+     */
     public abstract double getFinalCost();
+    /**
+     * creates a specific room
+     * @param room
+     */
     public abstract void createRoom(Baseroom room);
 }
