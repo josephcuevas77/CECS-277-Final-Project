d414521f8460299f0e02e4d08d5973baec66a94c
Joseph Cuevas <38505353+josephcuevas77@users.noreply.github.com>
2019-05-07 00:11:03 -0700

    Add files via upload

---
+++ FrontDeskAgent.java
@@ -0,0 +1,43 @@
+package FinalExam;
+
+import java.util.ArrayList;
+
+public class FrontDeskAgent {
+	
+	private ArrayList<Guest> guests = new ArrayList<Guest>();
+	private ArrayList<Reservation> reservations = new ArrayList<Reservation>();
+	private ArrayList<Reservation> waitlist = new ArrayList<Reservation>();
+
+	public boolean viewAvailability() {
+		
+	}
+	
+	public void addToWaitlist(Reservation r) {
+		
+	}
+	
+	public void removeFromWaitlist() {
+		
+	}
+	
+	public void updateGuestInfo() {
+		
+	}
+	
+	public boolean reserved() {
+		
+	}
+	
+	public void collectAmountDue() {
+		
+	}
+	
+	public void editGuestInfo() {
+		
+	}
+	
+	public static void main(String[] args) {
+
+	}
+
+}
