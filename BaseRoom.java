ba4b5a6aa63f0958ce46cbd0d25f4cc71d88139d
Justin Mabutas <justin.troy.mabutas@gmail.com>
2019-05-07 00:56:53 -0700

    Fixed stuff
    Fixed costs from returning ints to doubles.
    Added constructors.

--- BaseRoom.java
+++ BaseRoom.java
@@ -1,11 +1,23 @@
 public abstract class BaseRoom {
 
-    public BaseRoom()
+    public BaseRoom() {
+        description = "";
+        capacity = 0;
+        costPerHour = 0.0;
+        mealPlan = new MealPlan();
+    }
+    
+    public BaseRoom(String desc, int capa, double cost, MealPlan mp) {
+        description = desc;
+        capacity = capa;
+        costPerHour = cost;
+        mealPlan = mp;
+    }
     
     private String description;
     private int capacity;
     private double costPerHour;
-    private MealPlan MealPlan;
+    private MealPlan mealPlan;
 
     public abstract double getFinalCost();
     public abstract void createRoom(Baseroom room);
