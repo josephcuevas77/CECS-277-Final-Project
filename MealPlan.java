8f5e2bb2e9ebfa946ed9bbf801aa8af1dc76e250
Justin Mabutas <justin.troy.mabutas@gmail.com>
2019-05-07 03:06:21 -0700

    Updated version.
    Actually lowkey usable now. 
    Constructor requires a parameter indicating what tier of meal plan (Empty could be basic, but I didn't implement that yet)
    My idea: When upgrading meal plans, instantiate a new meal plan that has the meal plan that will be upgraded to.
    Example:
    currMealPlan = Basic
    upgrade("gold")
    currMealPlan = new MealPlan("gold")
    The upgrade function would pretty much be redefining the current meal plan, however you can't call new on an instantiated object (i dont think)

--- MealPlan.java
+++ MealPlan.java
@@ -1,44 +1,75 @@
-//Justin Mabutas
+import java.util.Scanner;
 
 public class MealPlan {
 
-    //$65
-    private static final String BASIC = "\n3 XL 1 Topping Gourmet Pizzas\n32L Soda Bottles";
-    //$75
-    private static final String BRONZE = "\n3 XL 2 Topping Gourmet Pizzas\n52L Soda Bottles\nSalad or bread-sticks";
-    //$90
-    private static final String SILVER = "\n3 XL 2 Topping Gourmet Pizzas\n52L Soda Bottles\nSalad\nBreak-sticks";
-
-
     public static void main(String[] args) {
-        MealPlan standard = new MealPlan(65.0, MealPlan.BASIC);
-        MealPlan bronze = new MealPlan(75.0, MealPlan.BRONZE);
-        MealPlan silver = new MealPlan(75.0, MealPlan.SILVER);
-        System.out.println(String.format("%s\n%s\n%s", standard, bronze, silver));
+    	MealPlan test = new MealPlan("basic");
+    	System.out.println(test.getDescrition());
     }
 
-    public MealPlan(double cost, String description) {
-        this.cost = cost;
-        this.description = description;
+    public MealPlan(String type) {
+    	switch(type) {
+    		case("basic"):
+    			for(int i = 0; i < 3; i++)
+    				addPizza(1);
+				break;
+    		case("bronze"):
+    			for(int i = 0; i < 3; i++)
+    				addPizza(2);
+				break;
+    		case("silver"):
+    			for(int i = 0; i < 3; i++)
+    				addPizza(3);
+				break;
+    		case("gold"):
+    			for(int i = 0; i < 3; i++)
+    				addPizza(3);
+				break;
+    		case("platinum"):
+    			for(int i = 0; i < 4; i++)
+    				addPizza(4);
+				break;
+    		default:
+    	}
+    	scan.close();
     }
-
+    
+    private Scanner scan = new Scanner(System.in);
+    
     private double cost;
-    private String description;
-    private String[] pizzaToppings;
-    private String[] wingFlavors;
-    private String[] iceCreamFlavors;
-    private String[] sodaBottles;
+    private String description = "";
+    private String[] pizzaToppings = "Cheese Pepperoni Ham Jalapeno Sausage Mushroom Pineapple Bell_Pepper Onion Garlic_Chicken".split(" ");
+    private String[] wingFlavors = "Mild_Spicy Diable Lemon_Pepper BBQ Sesame".split(" ");
+    private String[] iceCreamFlavors = "Chocolate_Fudge Vanilla_Bean Strawberry_Shortcake Choco-mint Butter_Pecan".split(" ");
+    private String[] sodaBottles = "Coca-Cola Diet_Coke Canada_Dry Orange_Crush Squirt Root_Beer".split(" ");
 
+    //These functions are used for the description
+    private void addPizza(int numToppings) {
+    	System.out.println(String.format("For your %d topping pizza :^D", numToppings));
+    	String temp = "Pizza\n Toppings: ";
+    	for(int i = 0; i < pizzaToppings.length; i++) {
+    		System.out.print(pizzaToppings[i] + "    ");
+    		if(i % 3 == 2) System.out.println("");
+    	}
+    	System.out.println("");
+    	for(int i = 0; i < numToppings; i++) {
+    		System.out.print(String.format("Enter topping %d: ", i+1));
+    		temp += scan.nextLine() + "\n";
+    	}
+    	description += temp + "\n";
+    }
+    
     public double getCost() {return cost;}
-    public void setCost(double cost) {this.cost = cost;}
     public String getDescrition() {return description;}
-    public void setDescription(String description) {this.description = description;}
+    
     public String[] getPizzaToppings() {return pizzaToppings;}
     public String[] getWingFlavors() {return wingFlavors;}
     public String[] getIceCreamFlavors() {return iceCreamFlavors;}
     public String[] getSodeBottles() {return sodaBottles;}
 
+    
+    
     public String toString() {
-        return String.format("Cost: %.2f \nDescription: %s", cost, description);
+        return String.format("Cost: %.2f \nDescription: \n%s", cost, description);
     }
 }
