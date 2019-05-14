afb32bf19e9d69041cdfe0ee139173ac025dc16d
Justin Mabutas <justin.troy.mabutas@gmail.com>
2019-05-14 00:46:32 -0700

    CONSIDER USING THESE CLASSES INSTEAD OF THE OTHER FACTORIES
    I think this is better
    -Justin c^:

---
+++ ConcreteRoomFactory.java
@@ -0,0 +1,25 @@
+package FinalExam;
+
+public class ConcreteRoomFactory extends BaseRoomFactory {
+
+	public static String[] ROOMS = {"Aqua World", "Small Party Room", "Medium Party Room", "Karaoke Lounge", "Adult Billiards Lounge"};
+	@Override
+	public BaseRoom createRoom(int num) {
+		switch(num) {
+			case(0): //Aqua World
+				return new AquaWorld();
+			case(1): //Small Party Room
+				return new SmallPartyRoom();
+			case(2): //Medium Party Room
+				return new MediumPartyRoom();
+			case(3): //Karaoke Lounge
+				return new KaraokeLounge();
+			case(4): //Adult Billiards Lounge
+				return new AdultBilliardsLounge();
+			default:
+				return null;
+			
+		}
+	}
+
+}
