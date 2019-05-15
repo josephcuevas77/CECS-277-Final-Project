06298f7cb98e582aaaa9cce4f6e4195eda02f4d7
Joseph Cuevas <38505353+josephcuevas77@users.noreply.github.com>
2019-05-14 22:10:53 -0700

    Delete FactoryAdultBilliardsLounge.java

--- FactoryAdultBilliardsLounge.java
+++
@@ -1,9 +0,0 @@
-
-public class FactoryAdultBilliardsLounge {
-	/**
-	 * createRoom function which returns the room as a BaseRoom
-	*/
-	public BaseRoom createRoom() {
-		return new KaraokeLounge();
-	}
-}
