e0cc511806267f90fe8ff66d3bdb9d11ea8e23b6
Joseph Cuevas <38505353+josephcuevas77@users.noreply.github.com>
2019-05-14 22:11:18 -0700

    Delete FactoryMediumPartyRoom.java

--- FactoryMediumPartyRoom.java
+++
@@ -1,8 +0,0 @@
-public class FactoryMediumPartyRoom {
-	/**
-	 * createRoom function which returns the room as a BaseRoom
-	*/
-	public BaseRoom createRoom() {
-		return new MediumPartyRoom();
-	}
-}
