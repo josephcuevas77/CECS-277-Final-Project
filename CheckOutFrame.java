7df8670cba7c83d50185e5a025909b6983d7611c
5toubun <50353828+5toubun@users.noreply.github.com>
2019-05-16 05:27:39 -0700

    Update CheckOutFrame.java

--- CheckOutFrame.java
+++ CheckOutFrame.java
@@ -26,12 +26,12 @@ public class CheckOutFrame extends JFrame{
 	private JLabel label2;
 	private JTextField textField1;
 	private JTextField textField2;
-	final private static int FRAME_WIDTH = 500;
+	final private static int FRAME_WIDTH = 550;
 	final private static int FRAME_HEIGHT = 500;
 	
 	ArrayList<JTextField> fields = new ArrayList<JTextField>();
 	ArrayList<JButton> buttons = new ArrayList<JButton>();
-	ArrayList<String> CheckedInGuests = new ArrayList<String>();
+	ArrayList<Guest> CheckedInGuests = new ArrayList<Guest>();
 	
 	
 	
@@ -46,7 +46,15 @@ public class CheckOutFrame extends JFrame{
 		
 		public void actionPerformed(ActionEvent event) {
 			if(event.getSource() == buttons.get(0)) {
-				
+				for (Reservation r : FrontDeskAgent.getReservations()) {
+					if (r.getGuest().equals(CheckedInGuests.get(list.getSelectedIndex()))) {
+						try {
+						r.getBaseRoom().updateCostOfAddOns(Integer.parseInt(label1.getText()));
+						r.getBaseRoom().updateDescriptionOfAddOns(label2.getText());
+						} 
+						catch (Exception e) {}
+					}
+				}
 			}
 		}
 	}
@@ -56,21 +64,24 @@ public class CheckOutFrame extends JFrame{
 		ActionListener listener = new ClickListener();
 		JPanel panel = new JPanel();
 		
-		list = new JList();
+		for (Reservation r : FrontDeskAgent.getReservations()) {
+			if (r.getGuest().getCheckInStatus()) {
+				CheckedInGuests.add(r.getGuest());
+			}
+		}
+		
+		list = new JList(CheckedInGuests.toArray());
 		list.setVisible(true);
 		panel.add(list);
 		
 		label1 = new JLabel("Additional Damage Charges: ");
 		label2 = new JLabel("Damage Description: ");
-		textField1 = new JTextField(100);
-		textField2 = new JTextField(100);
+		textField1 = new JTextField(25);
+		textField2 = new JTextField(25);
 		panel.add(label1);
 		panel.add(textField1);
 		panel.add(label2);
 		panel.add(textField2);
-		
-		double repairFunds = Integer.parseInt(textField1.getText());
-		String repairDescription = textField2.getText();
 		
 		button = new JButton("Finalize");
 		button.addActionListener(listener);
