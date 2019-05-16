88d8aaa326a61fef628b96a3ef0d8d0692ad2504
Joseph Cuevas <38505353+josephcuevas77@users.noreply.github.com>
2019-05-16 04:05:22 -0700

    idek anymore

--- EditReservationFrame.java
+++ EditReservationFrame.java
@@ -8,15 +8,33 @@ import java.util.ArrayList;
 public class EditReservationFrame extends JFrame {
 
 	private static final long serialVersionUID = 1L;
+	private JTextField textField;
+	private JCheckBox checkBox; 
+	private JComboBox<String> comboBox;
+	private JButton button;
+	private JPanel panel = new JPanel();
+	private JPanel panel1 = new JPanel();
+	private JPanel panel2 = new JPanel();
+	private JPanel panel3 = new JPanel();
+	private JPanel panel4 = new JPanel();
+	private JPanel panel5 = new JPanel();
+	private JPanel panel6 = new JPanel();
+	private JPanel panel7 = new JPanel();
+	private JPanel panel8 = new JPanel();
+	private JPanel panel9 = new JPanel();
+	private JPanel panel0 = new JPanel();
+	private JPanel panel01 = new JPanel();
 
-	final private static int FRAME_WIDTH = 800;
-	final private static int FRAME_HEIGHT = 400;
+	final private static int FRAME_WIDTH = 1920;
+	final private static int FRAME_HEIGHT = 540;
 
 	ArrayList<JButton> buttons = new ArrayList<JButton>();
 	ArrayList<JTextField> fields = new ArrayList<JTextField>();
 	ArrayList<JLabel> labels = new ArrayList<JLabel>();
-
-	JPanel panel2 = new JPanel();
+	private ArrayList<JComboBox<String>> comboBoxes = new ArrayList<JComboBox<String>>();
+	
+	private String[] guestInfo = {"Name", "Phone Number", "Address", "Date of Birth", "Email", "Address"};
+	private String[] creditCard = {"Name", "Number", "Security Code", "Expiration Date"};
 
 	public EditReservationFrame() {
 		createComponents();
@@ -32,19 +50,24 @@ public class EditReservationFrame extends JFrame {
 				String confirmNumOrGN = fields.get(0).getText();
 				for (Reservation r : FrontDeskAgent.getReservations()) {
 					if(confirmNumOrGN.equals(r.getGuest().getName()) || confirmNumOrGN.equals(Integer.toString(r.getConfirmNum()))){
-						panel2.add(new JLabel("Guest Information: "));
-						createJTextArea(r.getGuest().toString(),panel2);
-						createButtonIntoPanel("Delete Reservation",panel2);
-						panel2.setVisible(true);
+						createButtonIntoPanel("Delete Reservation",panel01);
+						panel1.setVisible(true);	panel2.setVisible(true);
+						panel3.setVisible(true);	panel4.setVisible(true);
+						panel5.setVisible(true);	panel6.setVisible(true);
+						panel7.setVisible(true);	panel8.setVisible(true);
+						panel9.setVisible(true);	panel0.setVisible(true);
+						panel01.setVisible(true);
 					}
 				}
 				for(Reservation r: FrontDeskAgent.getWaitList()) {
 					if(confirmNumOrGN.equals(Integer.toString(r.getConfirmNum()))){
-						panel2.add(new JLabel("Guest Information: "));
-						createJTextArea(r.getGuest().toString(),panel2);
-						createButtonIntoPanel("Delete Reservation",panel2);
-						panel2.setVisible(true);
-						
+						createButtonIntoPanel("Delete Reservation",panel01);
+						panel1.setVisible(true);	panel2.setVisible(true);
+						panel3.setVisible(true);	panel4.setVisible(true);
+						panel5.setVisible(true);	panel6.setVisible(true);
+						panel7.setVisible(true);	panel8.setVisible(true);
+						panel9.setVisible(true);	panel0.setVisible(true);
+						panel01.setVisible(true);
 					}
 				}
 			}
@@ -70,18 +93,63 @@ public class EditReservationFrame extends JFrame {
 
 	public void createComponents() {
 		ActionListener listener = new ClickListener();
-		JPanel panel = new JPanel();
 		createLabelAndTextField("Enter Confirmation Number/Guest Name: ",panel);
 		createButtonIntoPanel("Confirm",panel);
-		panel.add(panel2);
-		panel2.setVisible(false);
+		panel1.add(new JLabel("Guest Information: "));
+		for (int i=0; i<guestInfo.length; i++) {
+			createText(guestInfo[i], panel1);
+		}
+		
+		panel2.add(new JLabel("Credit Card Information: "));
+		for (int i=0; i<creditCard.length; i++) {
+			createText(creditCard[i], panel2);
+		}
+		
+		checkBox = new JCheckBox("Contact via Phone"); 
+		panel3.add(checkBox);
+		checkBox = new JCheckBox("Contact via Email"); 
+		panel3.add(checkBox);
+		
+		createComboBox(ConcreteRoomFactory.ROOMS, "Rooms: ", panel4, comboBoxes);
+		createComboBox(MealPlan.mealPlans, "Meal Plans: ", panel4, comboBoxes);
+		createComboBox(MealPlan.pizzaToppings, "Pizza Toppings: ", panel5, comboBoxes);
+		createComboBox(MealPlan.sodaBottles, "Soda Choices: ", panel5, comboBoxes);
+		createComboBox(MealPlan.iceCreamFlavors, "Ice Cream Flavors: ", panel6, comboBoxes);
+		createComboBox(MealPlan.wingFlavors, "Wing Flavors: ", panel6, comboBoxes);
+
+
+		panel7.add(new JLabel("Room: "));
+		comboBox = new JComboBox<String>(ConcreteRoomFactory.ROOMS);
+		panel7.add(comboBox);
+		comboBox = new JComboBox<String>(MealPlan.mealPlans);
+		panel8.add(comboBox);
+		comboBox = new JComboBox<String>(MealPlan.pizzaToppings);
+		panel8.add(comboBox);
+		comboBox = new JComboBox<String>(MealPlan.sodaBottles);
+		panel9.add(comboBox);
+		comboBox = new JComboBox<String>(MealPlan.iceCreamFlavors);
+		panel9.add(comboBox);
+		comboBox = new JComboBox<String>(MealPlan.wingFlavors);
+		panel0.add(comboBox);
+		
+		button = new JButton("Save");
+		panel01.add(button);
+		button = new JButton("Cancel");
+		panel01.add(button);
+		
+		panel1.setVisible(false);	panel2.setVisible(false);
+		panel3.setVisible(false);	panel4.setVisible(false);
+		panel5.setVisible(false);	panel6.setVisible(false);
+		panel7.setVisible(false);	panel8.setVisible(false);
+		panel9.setVisible(false);	panel0.setVisible(false);
+		panel01.setVisible(false);
+		
+		panel.add(panel1);	panel.add(panel2);	panel.add(panel3);
+		panel.add(panel4);	panel.add(panel5);	panel.add(panel6);
+		panel.add(panel7);	panel.add(panel8);	panel.add(panel9);
+		panel.add(panel0);	panel.add(panel01);
 		add(panel);
-	}
-	
-	public void createJTextArea(String tArea,JPanel panel){
-		JTextArea tA = new JTextArea();
-		tA.setText(tArea);
-		panel.add(tA);
+		
 	}
 
 	public void createNotifyGuestFrame(Guest g) {
@@ -100,7 +168,7 @@ public class EditReservationFrame extends JFrame {
 		JLabel label = new JLabel(lab);
 		labels.add(label);
 		panel.add(label);
-		JTextField field = new JTextField(50);
+		JTextField field = new JTextField(20);
 		fields.add(field);
 		panel.add(field);
 	}
@@ -111,5 +179,22 @@ public class EditReservationFrame extends JFrame {
 		buttons.add(button);
 		button.addActionListener(listener);
 		panel.add(button);
+	}
+	
+	public JComboBox<String> createComboBox(String[] options, String title, JPanel panel, ArrayList<JComboBox<String>> comboList) {
+		createLabel(title, panel);
+		JComboBox<String> comboBox = new JComboBox<String>(options);
+		comboList.add(comboBox);
+		panel.add(comboBox);
+		return comboBox;
+	}
+	
+	public void createText(String s, JPanel p) {
+		JPanel panel = new JPanel();
+		JLabel label = new JLabel(s);
+		JTextField textField = new JTextField(20);
+		panel.add(label);
+		panel.add(textField);
+		p.add(panel);
 	}
 }
