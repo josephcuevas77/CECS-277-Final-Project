6ed47dd0e0760d89c876274ff4dc2c7805ce96bb
Joseph Cuevas <38505353+josephcuevas77@users.noreply.github.com>
2019-05-16 01:55:44 -0700

    Add files via upload

---
+++ EditReservationFrame.java
@@ -0,0 +1,102 @@
+package FinalExam;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
+
+public class EditReservationFrame extends JFrame {
+
+	private static final long serialVersionUID = 1L;
+	
+	final private static int FRAME_WIDTH = 800;
+	final private static int FRAME_HEIGHT = 400;
+	
+	ArrayList<JButton> buttons = new ArrayList<JButton>();
+	ArrayList<JTextField> fields = new ArrayList<JTextField>();
+	ArrayList<JLabel> labels = new ArrayList<JLabel>();
+	
+	JPanel panel2 = new JPanel();
+	
+	public EditReservationFrame() {
+		createComponents();
+		setSize(FRAME_WIDTH,FRAME_HEIGHT);
+		setTitle("Edit Reservation");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setVisible(true);
+	}
+	
+	class ClickListener implements ActionListener{
+		public void actionPerformed(ActionEvent event) {
+			String confirmNumOrGN = fields.get(0).getText();
+			for (Guest g : FrontDeskAgent.getGuests()) {
+				if(confirmNumOrGN.contentEquals(g.getName())){
+					panel2.add(new JLabel("Guest Information: "));
+					createLabel(g.toString(),panel2);
+					createButtonIntoPanel("Delete Reservation",panel2);
+					panel2.setVisible(true);
+				}
+			}
+			for(Reservation r: FrontDeskAgent.getWaitList()) {
+				if(confirmNumOrGN.equals(Integer.toString(r.getConfirmNum()))){
+					panel2.add(new JLabel("Guest Information: "));
+					createLabel(r.getGuest().toString(),panel2);
+					createButtonIntoPanel("Delete Reservation",panel2);
+					panel2.setVisible(true);
+				}
+			}
+			if(event.getSource() == buttons.get(0)) {
+				for (Guest g : FrontDeskAgent.getGuests()) {
+					for(Reservation r: FrontDeskAgent.getWaitList()) {
+						if(g.getDate().equals(r.getGuest().getDate()) && g.getTime().equals(r.getGuest().getTime())) {
+							createNotifyGuestFrame(r.getGuest());
+						}
+					}
+				}
+			}
+		}
+	}
+	
+	public void createLabel(String lab, JPanel panel) {
+		ActionListener listener = new ClickListener();
+		JLabel label = new JLabel(lab);
+		labels.add(label);
+		panel.add(label);
+	}
+	
+	public void createComponents() {
+		ActionListener listener = new ClickListener();
+		JPanel panel = new JPanel();
+		createLabelAndTextField("Enter Confirmation Number/Guest Name: ",panel);
+		add(panel);
+		add(panel2);
+		panel2.setVisible(false);
+	}
+	
+	public void createNotifyGuestFrame(Guest g) {
+		JFrame notifyGuest = new JFrame();
+		JPanel panel = new JPanel();
+		createLabel("Now notifying waitlisted guest " + g.getName() + " of confirmed reservation", panel);
+		notifyGuest.setTitle("Notify Waitlist Guest");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setVisible(true);
+	}
+	
+	public void createLabelAndTextField(String lab, JPanel panel) {
+		ActionListener listener = new ClickListener();
+		JLabel label = new JLabel(lab);
+		labels.add(label);
+		panel.add(label);
+		JTextField field = new JTextField(50);
+		fields.add(field);
+		panel.add(field);
+	}
+	
+	public void createButtonIntoPanel(String butt, JPanel panel) {
+		ActionListener listener = new ClickListener();
+		JButton button = new JButton(butt);
+		buttons.add(button);
+		button.addActionListener(listener);
+		panel.add(button);
+	}
+}
